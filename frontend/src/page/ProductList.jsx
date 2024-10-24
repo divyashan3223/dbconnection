7719f2934bb552438c27eaf2a7857475cc6ee968
divyashan3223 <divyashan3223@gmail.com>
2024-10-24 11:53:48 +0530

    fet:Db connect frontend and server folde added

---
+++ frontend/src/page/ProductList.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const ProductList = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const res = await axios.get("http://localhost:5000/api/products");
+        setProducts(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchProducts();
+  }, []);
+
+  return (
+    <div>
+      <h1>Products</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product._id}>
+            {product.name} - ${product.price}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ProductList;
